92db58373a8b4695f1fe9de0f5b6f59a394e143d
Emetselch1984 <banzai.nippon2030@gmail.com>
2021-07-21 20:15:18 +0900

    #3[add] update action in event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,6 +13,14 @@ class EventsController < ApplicationController
       render json: event.errors,status: 422
     end
   end
+  def update
+    event = Event.find(params[:id])
+    if event.update(events_params)
+      render json: event
+    else
+      render json: event.errors,status: 422
+    end
+  end
   private
   def events_params
     params.require(:event).permit(:id,:name,:start,:end,:description,:color)
