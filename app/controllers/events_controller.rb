65bdb59db4398730841f85f0872ce8e3659ce83d
Emetselch1984 <banzai.nippon2030@gmail.com>
2021-07-21 20:07:21 +0900

    #1[add] create action in event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,4 +5,16 @@ class EventsController < ApplicationController
   def show
     render json: Event.find(params[:id])
   end
+  def create
+    event = Event.new(events_params)
+    if event.save
+      render json: event
+    else
+      render json: event.errors,status: 422
+    end
+  end
+  private
+  def events_params
+    params.require(:event).permit(:id,:name,:start,:end,:description,:color)
+  end
 end
